f8dfba4dac7606a84a8bace17fb2fa0dbfdbab09
iecetitiz <i.ecetitiz@gmail.com>
2022-11-28 17:26:32 +0300

    executable functions are added

---
+++ data_types/defstatement.py
@@ -0,0 +1,13 @@
+age = int(input("enter your age please: "))
+
+if age > 27:
+    def myfunction():
+        print("hello this is my function!!!")
+else:
+    def yourfunction():
+        print("hello this is your function!!!")
+
+
+yourfunction()
+myfunction()
+
